a0ef683053494e2a0de639585f6ad683cc40d933
Paras Malhotra <paras@insenseanalytics.com>
2023-10-26 15:22:20 +0530

    save cleaned data in a file and add ability to load cached

--- data_loader.py
+++ data_loader.py
@@ -9,40 +9,57 @@ import numpy as np
 class DataLoader:
     def __init__(self, directory: str, verbose: bool = True):
         self.verbose = verbose
-        # Find all CSV files in the specified directory
-        file_paths = glob.glob(os.path.join(directory, '*.csv'))
+        self.directory = directory
 
-        data_list = []
-        for file_path in file_paths:
-            df = pd.read_csv(file_path)
-            # df['source_file'] = os.path.basename(file_path)
-            data_list.append(df)
+    def load_and_clean_data(self, fresh: bool = False) -> None:
+        cleaned_data_path = os.path.join(self.directory, 'cleaned.csv')
 
-        # Load and concatenate data from all found CSV files with source info
-        self.data = pd.concat(data_list, ignore_index=True)
-        # Strip whitespace, convert to lowercase, and replace spaces with underscores in column names
-        self.data.columns = self.data.columns.str.strip().str.lower().str.replace(' ', '_')
+        if not fresh and os.path.exists(cleaned_data_path):
+            # Load cleaned data from file if it exists and fresh is False
+            self.data = pd.read_csv(cleaned_data_path)
+            if self.verbose:
+                print(f'Loaded cleaned data from {cleaned_data_path}')
+        else:
+            if os.path.exists(cleaned_data_path):
+                os.remove(cleaned_data_path)
+                if self.verbose:
+                    print(f'Deleted existing cleaned data at {cleaned_data_path}')
 
-    def cleanse_data(self) -> None:
-        initial_size = len(self.data)
-        self.data.replace([np.inf, -np.inf], np.nan, inplace=True)
-        # Removing rows with any missing values
-        self.data.dropna(inplace=True)
+            # Find all CSV files in the specified directory
+            file_paths = glob.glob(os.path.join(self.directory, '*.csv'))
 
-        # Filter numeric columns
-        numeric_data = self.data.select_dtypes(include=['number'])
-        # Capture columns with constant values (zero variance)
-        constant_columns = numeric_data.columns[numeric_data.std() == 0]
-        # Drop these columns from the original dataset
-        self.data.drop(columns=constant_columns, inplace=True)
+            data_list = []
+            for file_path in file_paths:
+                df = pd.read_csv(file_path)
+                data_list.append(df)
 
-        final_size = len(self.data)
-        rows_removed = initial_size - final_size
-        if self.verbose:
-            print(f"Removed {rows_removed} rows with missing values.")
-            # Print the columns dropped
-            if constant_columns.size > 0:
-                print("Dropped columns with constant values:", ", ".join(constant_columns))
+            # Load and concatenate data from all found CSV files with source info
+            self.data = pd.concat(data_list, ignore_index=True)
+            self.data.columns = self.data.columns.str.strip().str.lower().str.replace(' ', '_')
+
+            initial_size = len(self.data)
+            self.data.replace([np.inf, -np.inf], np.nan, inplace=True)
+            # Removing rows with any missing values
+            self.data.dropna(inplace=True)
+            # Filter numeric columns
+            numeric_data = self.data.select_dtypes(include=['number'])
+            # Capture columns with constant values (zero variance)
+            constant_columns = numeric_data.columns[numeric_data.std() == 0]
+            # Drop these columns from the original dataset
+            self.data.drop(columns=constant_columns, inplace=True)
+
+            final_size = len(self.data)
+            rows_removed = initial_size - final_size
+            if self.verbose:
+                print(f"Removed {rows_removed} rows with missing values.")
+                # Print the columns dropped
+                if constant_columns.size > 0:
+                    print("Dropped columns with constant values:", ", ".join(constant_columns))
+
+            self.add_binary_label(label_column='label', negative_class='BENIGN')
+            self.data.to_csv(cleaned_data_path, index=False)
+            if self.verbose:
+                print(f'Saved cleaned data to {cleaned_data_path}')
 
     def add_binary_label(self, label_column: str, negative_class: str) -> None:
         self.data['binary_label'] = self.data[label_column].apply(lambda x: 0 if x == negative_class else 1)
--- main.py
+++ main.py
@@ -11,8 +11,7 @@ def main(verbose=True) -> None:
     current_dir = os.path.dirname(os.path.abspath(__file__))
     data_dir = os.path.join(current_dir, 'data')
     loader = DataLoader(directory=data_dir, verbose=verbose)
-    loader.cleanse_data()
-    loader.add_binary_label(label_column='label', negative_class='BENIGN')
+    loader.load_and_clean_data(fresh=False)
     X_train, X_val, X_test, y_train, y_val, y_test = loader.split_data(original_label_column='label', features=None, test_size=0.3, val_size=0.0)
     
     if verbose:

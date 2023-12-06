eaebbfdf1f777e1ea61af41675b7204d5c2512f6
Paras Malhotra <paras@insenseanalytics.com>
2023-12-07 03:26:21 +0530

    Add neural network binary classifier

--- binary_classifier.py
+++ binary_classifier.py
@@ -1,5 +1,6 @@
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
+from sklearn.neural_network import MLPClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 from typing import Dict, Any
@@ -20,7 +21,8 @@ class BinaryClassifier:
             'Regularized GDA': QuadraticDiscriminantAnalysis(reg_param=0.7),
             'Logistic Regression': LogisticRegression(random_state=42, solver='liblinear', penalty='l2', fit_intercept=True, max_iter=1000, verbose=1 if self.verbose else 0),
             'Decision Tree': DecisionTreeClassifier(random_state=42, criterion='gini', splitter='best', max_depth=None, max_features=None),
-            'Random Forest': RandomForestClassifier(random_state=42, criterion='gini', max_depth=None, max_features='sqrt', n_jobs=-1, verbose=3 if self.verbose else 0)
+            'Random Forest': RandomForestClassifier(random_state=42, criterion='gini', max_depth=None, max_features='sqrt', n_jobs=-1, verbose=3 if self.verbose else 0),
+            'Neural Network': MLPClassifier(random_state=42, hidden_layer_sizes=(100,), batch_size=512, activation='relu', solver='adam', alpha=0.0001, max_iter=1000, learning_rate_init=0.001, early_stopping=True, verbose=1 if self.verbose else 0),
         }
         for name, model in models.items():
             file_path = os.path.join(self.model_dir, f'{name.replace(" ", "_").lower()}.joblib')
